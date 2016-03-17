33332cdc9f05260cbbb3d36f61ba8bf6f5b08015
Mihail <fsvymk@gmail.com>
2016-03-17 17:08:06 +0300

    before bitbucket repo created

--- main.cpp
+++ main.cpp
@@ -15,34 +15,47 @@ struct myStruct
     float fValue;
 };
 
-union eu{
-    struct entry{
-        quint32 x;
-        qint32  y;   // 64 bits
-        quint16 z;
-        quint16 v;
-        quint16 n;
-        quint16 t;   // 64 bits
-            // Total : 128 bits
-    };
+struct entry{
+    quint32 x;
+    qint32  y;   // 64 bits
+    quint16 z;
+    quint16 v;
+    quint16 n;
+    quint16 t;   // 64 bits
+        // Total : 128 bits
+};
 
+union eu{
+    entry e;
     char packed[16];
+    char mini[8];
 };
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
 
-    myStruct myS;
-    myS.iValue = 1;
-    myS.fValue = 2.0;
+    eu e;
 
-    myUnion myU;
-    myU.iValue = 1;
-    myU.fValue = 2.0;
+    char to[16]         = {1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 4, 0, 5, 0, 6, 0};
+    char mini_sample[8] = {7, 0, 0, 0, 8, 0, 0, 0};
 
-    cout << myS.iValue << " " << myS.fValue << endl;
-    cout << myU.iValue << " " << myU.fValue << endl;
+    memcpy(&e.packed, &to, 16);
 
-    return a.exec();
+    cout << "X = " << e.e.x << " ";
+    cout << "Y = " << e.e.y << " ";
+    cout << "T = " << e.e.t << " ";
+    cout << "Z = " << e.e.z << " ";
+    cout << "V = " << e.e.v << " ";
+    cout << "N = " << e.e.n << " ";
+    cout << "\n";
+
+    memcpy(&e.packed, &mini_sample, 8);
+
+    cout << "X = " << e.e.x << " ";
+    cout << "Y = " << e.e.y << " ";
+    cout << "\n";
+
+    //return a.exec();
 }
+
